cf055e637cd7e423fb0dd899d365b2e6e5a2c508
Craig Hamilton <craigh@quailholdings.com>
2020-03-31 09:49:50 -0700

    refactor structure of tests

--- tests/test_nhl.py
+++ tests/test_nhl.py
@@ -3,6 +3,12 @@
 import datetime
 import pickle
 
+import json
+import os
+from nhl_scraper import nhl
+import tests.mock_nhl as mock_nhl
+import datetime
+import pytest
 
 def test_teams(nhl_scraper):
     df = nhl_scraper.teams()
@@ -45,3 +51,53 @@ def test_pickle(nhl_scraper):
     new_dc = pickle.loads(tmp)
     assert(new_dc[17] == 2)
     assert(new_dc[11] == 0)
+
+@pytest.fixture
+def nhl_scraper():
+    s = nhl.Scraper()
+
+    # Put the mock adapter in so we don't make calls out
+    mock = mock_nhl.MockNhlEndpointAdapter()
+
+    cached_dates = [datetime.datetime(2018, 1, 14),
+                    datetime.datetime(2018, 1, 15),
+                    datetime.datetime(2018, 1, 16)]
+    for cached_date in cached_dates:
+        mock.add_date(cached_date)
+
+    s.set_endpoint_adapter(mock)
+    return s
+
+
+
+class MockNhlEndpointAdapter:
+    def __init__(self):
+        self.dir_path = os.path.dirname(os.path.realpath(__file__))
+        self.schedule_cache = {}
+        self.players_cache = None
+
+    def teams_endpoint(self):
+        fn = "sample.nhl.teams.json"
+        with open(self.dir_path + "/" + fn, "r") as f:
+            return json.load(f)
+
+    def schedule_endpoint(self, date):
+        if date in self.schedule_cache:
+            return self.schedule_cache[date]
+        else:
+            raise RuntimeError("{} is not in the schedule cache".format(date))
+
+    def add_date(self, date):
+        ds = date.strftime("%Y%m%d")
+        fn = "sample.nhl.schedule.{}.json".format(ds)
+        with open(self.dir_path + "/" + fn, "r") as f:
+            self.schedule_cache[date] = json.load(f)
+
+    def players_endpoint(self, team_ids):
+        if self.players_cache is None:
+            fn = "sample.nhl.players.json"
+            with open(self.dir_path + "/" + fn, "r") as f:
+                self.players_cache = json.load(f)
+        return self.players_cache
+
+
